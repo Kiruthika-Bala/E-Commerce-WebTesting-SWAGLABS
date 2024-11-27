05f25194ab2c83b91f024e8aae472dae5116d6c6
Kiruthika B <kiruthika51293@gmail.com>
2024-11-27 15:00:12 +0530

    Refactor: Verifying Cart

--- Search_cart update.py
+++ Search_cart update.py
@@ -9,7 +9,7 @@ driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.NAME,"password").send_keys("secret_sauce")
 driver.find_element(By.XPATH,"//input[@id='login-button']").click()
 time.sleep(5)
-
+#verifying the cart
 #search the desirable product
 driver.find_element(By.XPATH, "//div[normalize-space()='Sauce Labs Bolt T-Shirt']").click()
 driver.find_element(By.XPATH,"//button[@id='add-to-cart']").click()
