30860eaf31a5bccc36112f1ada38828cef228628
Kiruthika B <kiruthika51293@gmail.com>
2024-11-27 15:01:35 +0530

    Feat: Submitting Login Page

--- EC-Login.py
+++ EC-Login.py
@@ -5,6 +5,7 @@ driver= webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 driver.maximize_window()
 print(driver.title)
+#Submitting login page
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.NAME,"password").send_keys("secret_sauce")
 driver.find_element(By.XPATH,"//input[@id='login-button']").click()
