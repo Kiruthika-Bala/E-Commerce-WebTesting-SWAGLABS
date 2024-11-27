c648a3a303cacf74caa307a728b18c80bb03a79e
Kiruthika B <kiruthika51293@gmail.com>
2024-11-27 13:47:25 +0530

    Login Page Submitted

---
+++ EC-Login.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver= webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+driver.maximize_window()
+print(driver.title)
+driver.find_element(By.ID,"user-name").send_keys("standard_user")
+driver.find_element(By.NAME,"password").send_keys("secret_sauce")
+driver.find_element(By.XPATH,"//input[@id='login-button']").click()
+time.sleep(5)
